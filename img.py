efd0a48f40d0ae9a04a7b33ef9a1cf361b4d2963
Juan <juaniturriaga@fi.mdp.edu.ar>
2026-08-31 16:25:13 -0300

    correcciones y ampliaciones de funcionalidad ssim map

--- img.py
+++ img.py
@@ -2,16 +2,26 @@ import cv2 as cv
 import numpy as np
 import os
 
-def image_load(path, mode='color'):
+def image_load(path, mode='rgb'):
     #cargar una imagen desde un archivo
     map = {
-        'color': cv.IMREAD_COLOR,
+        'rgb': cv.IMREAD_COLOR,
+        'bgr': cv.IMREAD_COLOR,
         'grayscale': cv.IMREAD_GRAYSCALE,
         'unchanged': cv.IMREAD_UNCHANGED
     }
-    return cv.imread(path, map.get(mode, cv.IMREAD_COLOR))
+    img = cv.imread(path, map.get(mode,  map.get(mode)))
+    if mode == 'rgb' and img is not None:
+        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    return img
 
-def images_load (folder_path, mode='color', extensions=['.png', '.jpg', '.jpeg'], recursive=False, max_images=None, dim = (-1,-1), verbose=False):
+def image_save(path, image, mode='rgb'):
+    #guardar una imagen en un archivo
+    if mode == 'rgb':
+        image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+    cv.imwrite(path, image)
+    
+def images_load (folder_path, mode='rgb', extensions=['.png', '.jpg', '.jpeg'], recursive=False, max_images=None, dim = (-1,-1), verbose=False):
     #cargar todas las imagenes de una carpeta
     
     images = []
@@ -42,6 +52,13 @@ def images_load (folder_path, mode='color', extensions=['.png', '.jpg', '.jpeg']
         print(f"Cargadas {len(images)} imágenes desde {folder_path}")
         
     return np.array(images)
+
+def images_save (folder_path, images, mode='rgb', prefix='image_', start_index=0):
+    #guardar una lista de imagenes (numpy array) en una carpeta
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    for i, img in enumerate(images):
+        image_save(os.path.join(folder_path, f"{prefix}{start_index + i:06d}.png"), img, mode)
 
 def image_resize(image, width, height, interpolation='bilinear'):
     #redimensionar una imagen a un tamaño específico
@@ -79,14 +96,24 @@ def image_map_compare (img_orginal, img_test, method='diff'):
         return cv.add(img_orginal, img_test)
     else:
         raise ValueError(f"Invalid comparison method: {method}")
-
-if __name__ == "__main__":
-    image = image_load(".\\ds\\ds_xray_1024\\images_001\\images\\00000001_000.png")
-    print(f"Original Image Shape: {image.shape}")
-    resized_image = image_resize(image, 32, 32, interpolation='bicubic')
-    cv.imshow("Resized Image", resized_image)
-    cv.waitKey(0)
+    
+    
+def image_show (image, title="Image", color='rgb', wait_key=0):
+    #mostrar una imagen en una ventana
+    
+    if color == 'rgb':
+        image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+    
+    cv.imshow(title, image)
+    cv.waitKey(wait_key)
     cv.destroyAllWindows()
 
-    
-
+if __name__ == "__main__":
+    #image = image_load(".\\ds\\ds_xray_1024\\images_001\\images\\00000001_000.png")
+    image = image_load(".\\ds\\random_shapes_64\\00000000.png", mode='grayscale')
+    if image is not None:
+        print(f"Original Image Shape: {image.shape}")
+        resized_image = image_resize(image, 256, 256, interpolation='bicubic')
+        image_show(resized_image, title="Resized Image", color='grayscale', wait_key=0)
+    else:
+        print("Error: No se pudo cargar la imagen.")
--- img_ssim_map.py
+++ img_ssim_map.py
@@ -3,20 +3,24 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 from skimage.metrics import structural_similarity as ssim
 import cv2 as cv
-from img import image_load, image_resize
+from img import image_load, image_save, image_show, image_resize, images_load, images_save, images_resize
 
 
 
 def ssim_map_calculate(img_ref, img_test):
     """
-    Calcula el mapa de similitud estructural (SSIM) entre dos imágenes y genera un mapa de calor.
+    Calcula el mapa de similitud estructural (SSIM) entre dos imágenes.
 
     Args:
         img_ref (numpy.ndarray): Imagen de referencia.
         img_test (numpy.ndarray): Imagen de prueba.
 
     Returns:
-        None
+        score (float): Valor promedio del SSIM entre las dos imágenes.
+        ssim_map (numpy.ndarray): Mapa de similitud estructural entre las dos imágenes.
+            cada celda devuelve un valor entre -1 y 1, donde 1 indica una similitud perfecta.
+        
+        
     """
     
     if img_ref.shape != img_test.shape:
@@ -37,22 +41,32 @@ def ssim_map_calculate(img_ref, img_test):
         
     return score, ssim_map
 
+def ssim_maps_calculate(images_ref, images_test):
+    if len(images_ref) != len(images_test):
+        raise ValueError("Las listas de imágenes deben tener el mismo tamaño.")
 
-def img_ssim_map (img_ref, img_test, title=False, colorbar=False, save_to=None):
+    scores = []
+    ssim_maps = []
+    for img_ref, img_test in zip(images_ref, images_test):
+        score, ssim_map = ssim_map_calculate(img_ref, img_test)
+        scores.append(score)
+        ssim_maps.append(ssim_map)
+        
+    scores = np.array(scores)
+    ssim_maps = np.array(ssim_maps)
+
+    return scores, ssim_maps
+
+def ssim_color_map (mode='rgb'):
     """
-    Calcula el mapa de similitud estructural (SSIM) entre dos imágenes y genera un mapa de calor.
+    Genera un colormap personalizado para el mapa SSIM basado en el paper.
 
     Args:
-        img_ref (numpy.ndarray): Imagen de referencia.
-        img_test (numpy.ndarray): Imagen de prueba.
-        title (bool): Si es True, muestra el título con el score SSIM.
-        colorbar (bool): Si es True, muestra la barra de color.
-        save_to (str): Ruta para guardar la imagen del mapa de calor. Si es None, no se guarda.
+        mode (str, optional): Modo de color. puede ser 'rgb' o 'bgr'. Defaults to 'rgb'.
+
     Returns:
-        None
+        LinearSegmentedColormap: Colormap personalizado para SSIM.
     """
-    
-    score, ssim_map = ssim_map_calculate(img_ref, img_test)
     
     # Crea el colormap personalizado descrito en el paper
     # Nodos del gradiente (escala de -1.0 a 1.0 normalizada a 0.0 - 1.0 para matplotlib)
@@ -62,29 +76,85 @@ def img_ssim_map (img_ref, img_test, title=False, colorbar=False, save_to=None):
     #   1.0 (1.0 en el nodo) -> Blanco
     nodos = [0.0, 0.4999, 0.5, 1.0]
     # colores BGR (como los trabaja cv2)
-    colores = [ 
-        (0.0, 0.0, 1.0),  # Rojo (-1) 
-        (0.0, 1.0, 0.0),  # Verde (acercándose a 0 desde los negativos)
-        (0.0, 0.0, 0.0),  # Negro (exactamente 0)
-        (1.0, 1.0, 1.0)   # Blanco (1)
-    ]
+    
+    colors_modes = {
+        'bgr': [
+            (0.0, 0.0, 1.0),  # Rojo (-1) 
+            (0.0, 1.0, 0.0),  # Verde (acercándose a 0 desde los negativos)
+            (0.0, 0.0, 0.0),  # Negro (exactamente 0)
+            (1.0, 1.0, 1.0)   # Blanco (1)
+        ],
+        'rgb': [
+            (1.0, 0.0, 0.0),  # Rojo (-1) 
+            (0.0, 1.0, 0.0),  # Verde (acercándose a 0 desde los negativos)
+            (0.0, 0.0, 0.0),  # Negro (exactamente 0)
+            (1.0, 1.0, 1.0)   # Blanco (1)
+        ]
+    }
+    
+    colores = colors_modes[mode]    
+    ssim_cmap = LinearSegmentedColormap.from_list(f"SSIM_cmap_{mode}", list(zip(nodos, colores)))
+    
+    return ssim_cmap
 
-    ssim_cmap = LinearSegmentedColormap.from_list("SSIM_paper_cmap", list(zip(nodos, colores)))
+def image_ssim_map (ssim_map, save_to=None, mode='rgb'):
+    """
+    Genera un mapa de calor a partir de un mapa de similitud estructural (SSIM).
+
+    Args:
+        ssim_map (numpy.ndarray): Mapa de similitud estructural.
+        save_to (str): Ruta para guardar la imagen del mapa de calor. Si es None, no se guarda.
+        mode (str, optional): Modo de color. puede ser 'rgb' o 'bgr'. Defaults to 'rgb'.
+    Returns:
+        None
+    """
+    
+    cmap = ssim_color_map(mode)
     
     ssim_map = (ssim_map + 1.0) / 2.0  # Normalizar de -1.0 - 1.0 a 0.0 - 1.0
     
     #crear una imagen combinando el mapa SSIM con el colormap
-    heatmap_numpy = (ssim_cmap(ssim_map)[:, :, :3] * 255).astype(np.uint8)
+    heatmap_numpy = (cmap(ssim_map)[:, :, :3] * 255).astype(np.uint8)
 
     if save_to is not None:
-        cv.imwrite(save_to, cv.cvtColor(heatmap_numpy, cv.COLOR_RGB2BGR))
+        image_save(save_to, heatmap_numpy, mode=mode)
+    
+    return heatmap_numpy
 
-    return score, heatmap_numpy, ssim_cmap
+
+def images_ssim_map(ssim_maps, save_to=None, mode='rgb'):
+    """
+    Genera mapas de calor a partir de una lista de mapas de similitud estructural (SSIM).
+
+    Args:
+        ssim_maps (numpy.ndarray): Lista de mapas de similitud estructural.
+        save_to (str): Ruta para guardar las imágenes del mapa de calor. Si es None, no se guarda.
+        mode (str, optional): Modo de color. puede ser 'rgb' o 'bgr'. Defaults to 'rgb'.
+    Returns:
+        numpy.ndarray: Lista de mapas de calor generados a partir de los mapas SSIM.
+    """
+
+    cmap = ssim_color_map(mode)
+    
+    heatmaps = []
+    for i, ssim_map in enumerate(ssim_maps):
+        #crear una imagen combinando el mapa SSIM con el colormap
+        ssim_map = (ssim_map + 1.0) / 2.0  # Normalizar de -1.0 - 1.0 a 0.0 - 1.0
+        heatmap_numpy = (cmap(ssim_map)[:, :, :3] * 255).astype(np.uint8)
+            
+        if save_to is not None:
+            image_save(f"{save_to}_{i}.png", heatmap_numpy, mode=mode)
+        
+        heatmaps.append(heatmap_numpy)
+
+    return np.array(heatmaps)
 
 
-def plot_img_ssim (heat_map, score, ssim_cmap, vmin=-1, vmax=1):
+def plot_ssim_map (ssim_map, score):
     # Asegurar que el rango vaya estrictamente de -1 a 1 para que el colormap se alinee correctamente
-    img_plot = plt.imshow(heat_map, cmap=ssim_cmap, vmin=vmin, vmax=vmax)
+    
+    cmap = ssim_color_map('rgb')
+    img_plot = plt.imshow(ssim_map, cmap=cmap, vmin=-1, vmax=1)
 
     # Añadir la barra de color a la derecha
     plt.colorbar(img_plot, label='Índice SSIM')
@@ -92,62 +162,81 @@ def plot_img_ssim (heat_map, score, ssim_cmap, vmin=-1, vmax=1):
     plt.axis('off')
 
     # Guardar la imagen o mostrarla
-    #plt.savefig('heatmap_ssim.png', bbox_inches='tight')
     plt.show()
 
-    #guardar el plot en numpy
-    plt.savefig('heatmap_ssim-con matplotlib.png', bbox_inches='tight')
-    #heatmap_numpy = np.array(plt.gcf().canvas.renderer.buffer_rgba())
+
+def _test_arrays():
+    images_originales = images_load('ds/random_shapes_64/', mode='rgb', extensions=['.png'], recursive=False, max_images=3)
+    
+    images_r = images_resize(images_originales, 512, 512, interpolation='bicubic')
+    images_t = images_resize(images_originales, 512, 512, interpolation='nearest')
+    
+    scores, ssim_maps = ssim_maps_calculate(images_r, images_t)
+    
+    images_m = images_ssim_map(ssim_maps, save_to=None, mode='rgb')
+    
+    for i in range(scores.shape[0]):
+        print(f"Imagen {i}: SSIM Score: {scores[i]:.4f} shape: {ssim_maps[i].shape} MAX: {np.max(ssim_maps[i]):.4f}  MIN: {np.min(ssim_maps[i]):.4f}")
+    
+    images_save('heatmaps_ssim', images_m, mode='rgb', prefix='heatmap_', start_index=0)
+    images_save('images_referencia', images_r, mode='rgb', prefix='ref_', start_index=0)
+    images_save('images_prueba', images_t, mode='rgb', prefix='test_', start_index=0)
     
     
+    
+def _test_single():
+    img_original = image_load('ds/random_shapes_64/00000000.png', mode='rgb')
+    
+    img_referencia = image_resize(img_original, 512, 512, interpolation='bicubic')
+    img_prueba = image_resize(img_original, 512, 512, interpolation='nearest')
+    
+    score, ssim_map = ssim_map_calculate(img_referencia, img_prueba)
+    
+    print(f"SSIM Score: {score:.4f} shape: {ssim_map.shape} MAX: {np.max(ssim_map):.4f}  MIN: {np.min(ssim_map):.4f}")
+            
+    heatmap = image_ssim_map(ssim_map, save_to='heatmap_ssim.png', mode='rgb')
+    
+    image_show(heatmap)
 
 
 if __name__ == "__main__":
+    
+    _test_arrays()
+    
+    
+    
+def garbage2():
 
     # 1. Cargar las dos imágenes (deben tener el mismo tamaño)
     # El paper asume que las imágenes se codifican en el espacio de color sRGB y no aplica transformaciones
     img_referencia = image_load('ds/ds_xray_1024/images_001/images/00000001_000.png')
-    img_prueba = image_load('ds/ds_xray_1024/images_001/images/00000001_001.png')
+    #img_prueba = image_load('ds/ds_xray_1024/images_001/images/00000001_001.png')
+    #invertir imagen de referencia
+    img_prueba = cv.bitwise_not(img_referencia)
+    
+    
+    
+    
 
     #img_referencia = image_load('ds/random_shapes_64/00000000.png')
     #img_prueba = image_load('ds/random_shapes_64/00000000.png')
-
     #img_referencia = image_resize(img_referencia, 512, 512, interpolation='bicubic')
     #img_prueba = image_resize(img_prueba, 512, 512, interpolation='nearest')
-    print (f'Image referencia shape: {img_referencia.shape}, Image prueba shape: {img_prueba.shape}')
+        
+    #print (f'Image referencia shape: {img_referencia.shape}, Image prueba shape: {img_prueba.shape}')
 
-
-    # Crea el colormap personalizado descrito en el paper
-    # Nodos del gradiente (escala de -1.0 a 1.0 normalizada a 0.0 - 1.0 para matplotlib)
-    # -1.0 (0.0 en el nodo) -> Rojo
-    # < 0.0 (~0.499 en el nodo) -> Verde
-    #   0.0 (0.5 en el nodo) -> Negro
-    #   1.0 (1.0 en el nodo) -> Blanco
-    nodos = [0.0, 0.4999, 0.5, 1.0]
-    # colores RGB
-    colores = [ 
-        (1.0, 0.0, 0.0),  # Rojo (-1) 
-        (0.0, 1.0, 0.0),  # Verde (acercándose a 0 desde los negativos)
-        (0.0, 0.0, 0.0),  # Negro (exactamente 0)
-        (1.0, 1.0, 1.0)   # Blanco (1)
-    ]
-
-    ssim_cmap_rgb = LinearSegmentedColormap.from_list("SSIM_paper_cmap", list(zip(nodos, colores)))
-
-
-    score, heatmap, ssim_cmap = img_ssim_map(img_referencia, img_prueba, title=True, colorbar=True, save_to='heatmap_ssim_v2-concv.png')
+    #calcula el mapa ssim y el score
+    #score, ssim_map = ssim_map_calculate(img_referencia, img_prueba)
+    
+    #print(f"SSIM Score: {score:.4f} shape: {ssim_map.shape} MAX: {np.max(ssim_map):.4f}  MIN: {np.min(ssim_map):.4f}")
+            
+    #heatmap = img_ssim_map(ssim_map, save_to='heatmap_ssim_v2-concv.png', mode='rgb')
+    #image_show(heatmap)
+    
+    #plot_ssim_map(ssim_map, score)
     
     
-    # BGR to rgb
-    heatmap_rgb = cv.cvtColor(heatmap, cv.COLOR_BGR2RGB)
-    heatmap_puro = (heatmap_rgb/255) * 2 -1
     
-    plot_img_ssim (heatmap_rgb, score, ssim_cmap_rgb, -1, 1)
-    
-    
-    #cv.imshow("Heatmap SSIM", heatmap)
-    #cv.waitKey(0)
-    #cv.destroyAllWindows()
     
 
 
